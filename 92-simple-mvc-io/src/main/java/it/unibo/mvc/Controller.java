a926c34a2ff43c3a8acc4ad632897e7fbd807120
Matteo Botteghi <matteo.botteghi6@studio.unibo.it>
2024-11-20 19:02:05 +0100

    Inizio es92 e aggiunta file .gitignore

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,62 @@
 package it.unibo.mvc;
 
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String HOME = System.getProperty("user.home");
+    private static final String DEFAULT_FILE = "output.txt";
+
+    private File currentFile = new File(HOME + File.separator + DEFAULT_FILE);
+
+    /*public Controller() {
+        String userHome = System.getProperty("user.home") + File.separator;
+        currentFile = new File(userHome, "output.txt");
+    }*/
+
+
+    public void setCurrentFile(File file) {
+        final File parent = file.getParentFile();
+        if(parent.exists()){
+            currentFile = file;
+        } else {
+            throw new IllegalArgumentException("non puoi salvare su una cartella che non esiste")
+        }
+    }
+
+
+    public File getCurrentFile() {
+        return currentFile;
+    }
+
+
+    public String getPath(File file) {
+        return file.getAbsolutePath();
+    }
+
+
+    public void saveContentToFile(String content) throws IOException {
+        if (currentFile == null) {
+            throw new IllegalStateException("No current file set.");
+        }
+
+
+        try (PrintStream ps = new PrintStream(currentFile , StandardCharsets.UTF_8)) {
+            ps.println(content);
+        } catch (IOException e) {
+            throw new IOException("Error wring on file: " + currentFile.getAbsolutePath(), e);
+        }
+
+
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,19 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +21,47 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    private final JFrame frame = new JFrame("My first java graphical interface");
+
+    public SimpleGUI(final Controller ctrl) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        frame.setContentPane(canvas);
+
+        final JTextArea textArea = new JTextArea("");
+        canvas.add(textArea, BorderLayout.CENTER);
+
+        final JButton saveButton = new JButton("save");
+        canvas.add(saveButton, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent event){
+                try {
+                    ctrl.saveContentToFile(textArea.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(),"error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        //frame size and location
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int)screen.getWidth();
+        final int sh = (int)screen.getHeight();
+        frame.setSize( sw/2, sh/2);
+        frame.setLocationByPlatform(true);
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 
 }
